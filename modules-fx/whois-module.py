21cb6337f70fc3a6928631cb62d59a03ffb7cf85
Fernan Fortich <ffchequemarca@gmail.com>
2023-06-21 11:42:36 -0500

    Se añade la consulta de whois a traves de Ips, tambien se añade manejo de excepciones en el modulo

--- modules-fx/whois-module.py
+++ modules-fx/whois-module.py
@@ -1,9 +1,24 @@
 #Recordar instalar whois en la vm donde vaya a correr el servicio web
 import whois
+from ipwhois import IPWhois
 
-def get_whois_info(url):
+#    except Exception as e:
+#        return f"Error retrieving WHOIS information: {e}"
+
+def get_whois_info_domain(url):
+    w = whois.query(url)
+    return str(w.__dict__).replace(", '"," \n'")
+
+
+def get_whois_info_ip(ip):
+    w = IPWhois(ip)
+    return str(w.lookup_whois()).replace(", '"," \n'")
+
+def get_whois_info(asset):
     try:
-        w = whois.query(url)
-        return str(w.__dict__).replace(", '"," \n'")
-    except Exception as e:
-        return f"Error retrieving WHOIS information: {e}"
+        return get_whois_info_domain(asset)
+    except Exception as e1:
+        try:
+            return get_whois_info_ip(asset)
+        except Exception as e2:
+            return f"Error retrieving WHOIS information: {e1} \nError retrieving WHOIS information: {e2}"
