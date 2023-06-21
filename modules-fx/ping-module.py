002e5a2e06e927d930ac630baabcde44c477ea4b
Fernan Fortich <ffchequemarca@gmail.com>
2023-06-21 03:14:53 -0500

    Se deja la funcionalidad de ICMP Checker en marcha, falta generar el .tex para reporte

---
+++ modules-fx/ping-module.py
@@ -0,0 +1,16 @@
+from pythonping import ping
+
+#Corroboramos el icmp
+def check_icmp_availability(target):
+    try:
+        response = ping(target, count=4)
+        if response.success():
+            return f"ICMP protocol is available on {target} \n" + str(response)
+            
+        else:
+            return f"ICMP protocol is not available on {target} \n" + str(response)
+            
+    except Exception as e:
+        return f"An error occurred while checking ICMP availability: {str(e)}"
+        
+#Gestionamos el archivo.tex para la generacion de reportes.
