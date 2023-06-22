97ddba6007a6789a3000e3e6c4014e48075db43d
Fernan Fortich <ffchequemarca@gmail.com>
2023-06-22 17:51:57 -0500

    Version que logra enumerar algunos directorios pero que aun no logra terminar la funcion

--- modules-fx/wfuzz-module.py
+++ modules-fx/wfuzz-module.py
@@ -2,7 +2,7 @@ import wfuzz
 
 def get_direnum_info(target):
     enumtable=""
-    for r in wfuzz.fuzz(url=str(target.strip()+"/FUZZ"), hc=[404], payloads=[("file",dict(fn="./payloads/directory-list-2.3-medium.txt"))]):
+    for r in wfuzz.fuzz(url=str(target.strip()+"/FUZZ"),scanmode=True ,hc=[404], payloads=[("file",dict(fn="./payloads/directory-list-2.3-medium.txt"))]):
         print(r)
         enumtable=enumtable + str(r) + "\n"
     return enumtable
